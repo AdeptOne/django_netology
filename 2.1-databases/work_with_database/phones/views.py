371760eac3925074417ab97fab4131aaac76569c
timvolzh <nigaman95@gmail.com>
2022-03-02 17:39:19 +0300

    Phones done

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+import phones
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +11,19 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phone_objects = Phone.objects.all()
+    phones = [p for p in phone_objects]
+    print(phones)
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = (Phone.objects.filter(slug=slug))[0]
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
